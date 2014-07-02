170521c32bc2b012c923002779da10d44848405c
mstarniciuc <mstarniciuc@mstarniciuc-System-Product-Name>
2014-07-02 12:18:53 +0300

    GA to header

--- mobile.php
+++ mobile.php
@@ -67,6 +67,38 @@ $xml = loadFromXML();
 		time = 0;
 	});
 		</script>
+		<script>
+			var _gaq = _gaq || [];
+				  _gaq.push(['_setAccount', 'UA-52113605-1']);
+				  _gaq.push(['_trackPageview']);
+
+				  (function() {
+					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+				  })();
+
+
+				(function(i, s, o, g, r, a, m) {
+					i['GoogleAnalyticsObject'] = r;
+					i[r] = i[r] || function() {
+
+						(i[r].q = i[r].q || []).push(arguments)
+					}, i[r].l = 1 * new Date();
+					a = s.createElement(o),
+							m = s.getElementsByTagName(o)[0];
+					a.async = 1;
+					a.src = g;
+					m.parentNode.insertBefore(a, m)
+
+				})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+
+
+				ga('create', 'UA-52113605-1', 'auto');
+
+				ga('send', 'pageview');
+		</script>
 	</head>
 
 	<body class="no-underline">
@@ -879,42 +911,6 @@ $xml = loadFromXML();
 		<script src="js/estimator.js"></script>
 
 		<!-- Segment.io analytics -->
-		<script>
-
-				var _gaq = _gaq || [];
-				  _gaq.push(['_setAccount', 'UA-52113605-1']);
-				  _gaq.push(['_trackPageview']);
-
-				  (function() {
-					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-				  })();
-
-
-				(function(i, s, o, g, r, a, m) {
-					i['GoogleAnalyticsObject'] = r;
-					i[r] = i[r] || function() {
-
-						(i[r].q = i[r].q || []).push(arguments)
-					}, i[r].l = 1 * new Date();
-					a = s.createElement(o),
-							m = s.getElementsByTagName(o)[0];
-					a.async = 1;
-					a.src = g;
-					m.parentNode.insertBefore(a, m)
-
-				})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-
-
-				ga('create', 'UA-52113605-1', 'auto');
-
-				ga('send', 'pageview');
-
-
-
-		</script>
 		<script>
 			$("#noLogin").click(function() {
 				$(".no-login").empty();
